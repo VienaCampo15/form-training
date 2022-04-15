24fecd939b1825ebfed263e2086af91b7b394def
VienaCampo15 <vienacampo15@gmail.com>
2022-04-14 20:59:40 -0500

    Traer arreglo del localStorage

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,44 @@
-const users = []
-const lis = document.getElementById("div-li")
-document.getElementById("form").addEventListener('submit', function(event) {
+let users = [];
+const lis = document.getElementById("div-li");
+
+const name = document.getElementById("name")
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+
+document.getElementById("form").addEventListener("submit", function (event) {
      event.preventDefault();
 
      const user = {
-          "name": document.getElementById("name").value,
-          "email": document.getElementById("email").value,
-          "password": document.getElementById("password").value
-     }
+          name: name.value,
+          email: email.value,
+          password: password.value,
+     };
 
-     users.push(user)
+     users.push(user);
+     console.log(users);
+
+     addUserToDOM(user)
+
+     localStorage.setItem("users", JSON.stringify(users));
+
+     name.value = ''
+     email.value = ''
+     password.value = ''
+});
+
+users = JSON.parse(localStorage.getItem("users")) || [];
+
+users.forEach((user) => {
+     addUserToDOM(user)
+});
+
+function addUserToDOM(user){
      const string = `<p class = "name"> Nombre: ${user.name} </p> 
      <p class="email"> Email: ${user.email}</p>
-     <p class ="password"> Password: ${user.password}</p>`
+     <p class ="password"> Password: ${user.password}</p>`;
+
      const li = document.createElement("li");
-     li.innerHTML = string
-     console.log(users)
-     
-     lis.appendChild(li)
+     li.innerHTML = string;
 
-     localStorage.setItem('users', JSON.stringify(users));
-})
-
-
-
-
-
-
-
-  
+     lis.appendChild(li);
+}
